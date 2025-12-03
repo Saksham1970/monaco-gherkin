20525552e3144ef05158a862b0768953bf236b9b
Saksham Gupta <saksham1970@gmail.com>
2025-12-04 00:56:11 +0530

    Basic Gherkin integration Done

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@ import * as monaco from 'monaco-editor';
 import { configure } from '@cucumber/monaco';
 import { buildStepDocuments, jsSearchIndex } from '@cucumber/suggest';
 import { ExpressionFactory, ParameterTypeRegistry } from '@cucumber/cucumber-expressions';
-import { getGherkinCompletionItems } from '@cucumber/language-service';
 
-// Initialize Gherkin language support
 async function initializeGherkin() {
   // Create ExpressionFactory with ParameterTypeRegistry
   const parameterTypeRegistry = new ParameterTypeRegistry();
@@ -14,12 +12,14 @@ async function initializeGherkin() {
   const expressions = [
     expressionFactory.createExpression('I have {int} cukes in my belly'),
     expressionFactory.createExpression('there are {int} blind mice'),
-    expressionFactory.createExpression('Given I have {int} cukes'),
-    expressionFactory.createExpression('When I eat {int} cukes'),
-    expressionFactory.createExpression('Then I should have {int} cukes'),
+    expressionFactory.createExpression('I have {int} cukes'),
+    expressionFactory.createExpression('I eat {int} cukes'),
+    expressionFactory.createExpression('I should have {int} cukes'),
   ];
 
   // Build step documents and create search index
+  // This is required for autocomplete to work - the index needs to be populated
+  // with step documents that match step texts to expressions
   const stepTexts = [
     'I have 42 cukes in my belly',
     'I have 96 cukes in my belly',
@@ -27,75 +27,10 @@ async function initializeGherkin() {
   ];
   const docs = buildStepDocuments(stepTexts, expressions);
   const index = jsSearchIndex(docs);
-
-  // Configure Monaco with Gherkin support
+  
   configure(monaco, index, expressions);
 
-  // Add Gherkin keyword completions by enhancing the existing provider
-  // We need to register a new provider that includes both keywords and step completions
-  const gherkinKeywords = [
-    { label: 'Feature:', insertText: 'Feature: ${1:Feature Name}\n  ${2:Description}', kind: monaco.languages.CompletionItemKind.Keyword },
-    { label: 'Scenario:', insertText: 'Scenario: ${1:Scenario Name}\n    ${2}', kind: monaco.languages.CompletionItemKind.Keyword },
-    { label: 'Scenario Outline:', insertText: 'Scenario Outline: ${1:Scenario Name}\n    ${2}', kind: monaco.languages.CompletionItemKind.Keyword },
-    { label: 'Background:', insertText: 'Background:\n    ${1}', kind: monaco.languages.CompletionItemKind.Keyword },
-    { label: 'Given', insertText: 'Given ${1:step}', kind: monaco.languages.CompletionItemKind.Keyword },
-    { label: 'When', insertText: 'When ${1:step}', kind: monaco.languages.CompletionItemKind.Keyword },
-    { label: 'Then', insertText: 'Then ${1:step}', kind: monaco.languages.CompletionItemKind.Keyword },
-    { label: 'And', insertText: 'And ${1:step}', kind: monaco.languages.CompletionItemKind.Keyword },
-    { label: 'But', insertText: 'But ${1:step}', kind: monaco.languages.CompletionItemKind.Keyword },
-  ];
-
-  // Register enhanced completion provider that includes keywords
-  // Note: This will override the one from configure(), but we'll include step completions too
-  monaco.languages.registerCompletionItemProvider('gherkin', {
-    provideCompletionItems: function (model, position) {
-      const gherkinSource = model.getValue();
-      const word = model.getWordUntilPosition(position);
-      
-      // Filter keyword completions based on current word
-      const prefix = word.word.toLowerCase();
-      const keywordCompletions = gherkinKeywords
-        .filter(keyword => keyword.label.toLowerCase().startsWith(prefix))
-        .map(keyword => ({
-          ...keyword,
-          range: {
-            startLineNumber: position.lineNumber,
-            endLineNumber: position.lineNumber,
-            startColumn: word.startColumn,
-            endColumn: word.endColumn,
-          },
-          insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-        }));
-
-      // Get step completions from the language service
-      const completionItems = getGherkinCompletionItems(
-        gherkinSource,
-        position.lineNumber - 1,
-        index
-      );
-      
-      const stepCompletions = completionItems
-        .filter((ci) => ci.insertText !== undefined)
-        .map((ci) => ({
-          label: ci.label,
-          kind: monaco.languages.CompletionItemKind.Text,
-          insertText: ci.insertText!,
-          insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-          range: {
-            startLineNumber: position.lineNumber,
-            endLineNumber: position.lineNumber,
-            startColumn: word.startColumn,
-            endColumn: word.endColumn,
-          },
-        }));
-
-      return {
-        suggestions: [...keywordCompletions, ...stepCompletions],
-      };
-    },
-  });
-
-  console.log('Gherkin language support configured');
+  console.log('Gherkin language support configured using language service');
 }
 
 // Initialize Monaco Editor with Gherkin support
